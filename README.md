5158ad827edaac9f7ae32a4c08c27b3bdbc3caaa
Shoji Kunitomo <shoji.kunitomo@gmail.com>
2020-05-05 02:38:19 +0000

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# php-hello
+# php-sample-application
+
+This is a sample application using php.
+
+## CLI
+
+### How to run php files
+
+```bash
+php main.php
+```
+
+## Debug
+
+Please check launch.json.
+
+## Tools
+
+### composer
+
+
---
+++ main.php
@@ -0,0 +1,28 @@
+<?php
+
+echo "FizzBuzz問題";
+echo PHP_EOL;
+
+foreach(range(1, 100) as $i)
+{
+    if ($i % 3 == 0 && $i % 5 == 0)
+    {
+        echo "FizzBuzz";
+    }
+    else if ($i % 3 == 0)
+    {
+        echo "Fizz";
+    }
+    else if ($i % 5 == 0)
+    {
+        echo "Buzz";
+    }
+    else
+    {
+        echo $i;
+    }
+
+    echo PHP_EOL;
+}
+
+?>
